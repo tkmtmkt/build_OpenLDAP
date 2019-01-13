a8cd9f375b91630e3b964f229735090a05b5b38c
Takamatsu Makoto <tkmtmkt@gmail.com>
2019-01-14 02:28:59 +0900

    add: shell for makeing archive file

--- docker/build.sh
+++ docker/build.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-SCRIPT_DIR=$(cd $(dirname $0);pwd)
+SCRIPT_DIR=$(cd $(dirname ${BASH_SOURCE:-$0});pwd)
+
+TARGET=openldap
 
 case "$1" in
   centos6|ubuntu1404)
-    pushd $SCRIPT_DIR
-    docker-compose run --rm $1 /opt/openldap/workspace/scripts/build-all.sh
+    pushd ${SCRIPT_DIR}
+    docker-compose run --rm $1 /opt/${TARGET}/workspace/scripts/build-all.sh
     popd
     ;;
   *)
--- docker/exec.sh
+++ docker/exec.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-SCRIPT_DIR=$(cd $(dirname $0);pwd)
+SCRIPT_DIR=$(cd $(dirname ${BASH_SOURCE:-$0});pwd)
 
 case "$1" in
   centos6|ubuntu1404)
-    pushd $SCRIPT_DIR
+    pushd ${SCRIPT_DIR}
     docker-compose run --rm $1 /bin/bash
     popd
     ;;
